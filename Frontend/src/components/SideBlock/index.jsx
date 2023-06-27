dd3a1366b7594e218303d8e7fa47afed5cfc59e1
LarinNikita <larin_nikita_m@mail.ru>
2023-06-27 15:24:11 +0700

    tags modal

--- Frontend/src/components/SideBlock/index.jsx
+++ Frontend/src/components/SideBlock/index.jsx
@@ -3,11 +3,11 @@ import styles from "./SideBlock.module.scss";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 
-export const SideBlock = ({ title, children }) => {
+export const SideBlock = ({ title, button, children }) => {
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography variant="h6" classes={{ root: styles.title }}>
-        {title}
+        {title} {button}
       </Typography>
       {children}
     </Paper>
--- Frontend/src/components/TagsBlock.jsx
+++ Frontend/src/components/TagsBlock.jsx
@@ -11,31 +11,90 @@ import Skeleton from "@mui/material/Skeleton";
 import { SideBlock } from "./SideBlock";
 import { Link } from "react-router-dom";
 
+import { Box, Chip, Divider, IconButton, Modal, Stack, Tooltip, Typography } from "@mui/material";
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+
 export const TagsBlock = ({ items, isLoading = true }) => {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+
+    minHeight: '50%',
+    // maxHeight: 500,
+
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 2,
+    borderRadius: '6px',
+    border: '1px solid #dedede',
+    // overflowX: 'auto',
+  };
+
   return (
-    <SideBlock title="Тэги">
-      <List>
-        {(isLoading ? [...Array(5)] : items).map((name, i) => (
-          <Link
-            key={i}
-            style={{ textDecoration: "none", color: "black" }}
-            to={`/tags/${name}`}
+    <>
+      <SideBlock
+        title="Тэги"
+        button={
+          <Tooltip title="Все тэги">
+            <IconButton onClick={handleOpen}>
+              <OpenInFullIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+        }
+      >
+        <List>
+          {(isLoading ? [...Array(5)] : items.slice(-5)).map((name, i) => (
+            <Link
+              key={i}
+              style={{ textDecoration: "none", color: "black" }}
+              to={`/tags/${name}`}
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <TagIcon />
+                  </ListItemIcon>
+                  {isLoading ? (
+                    <Skeleton width={100} />
+                  ) : (
+                    <ListItemText primary={name} />
+                  )}
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </SideBlock>
+      <Modal open={open} >
+        <Box sx={style}>
+          <Typography variant="h6" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
+            Список тэгов
+            <IconButton onClick={handleClose}>
+              <CloseFullscreenIcon fontSize="small" />
+            </IconButton>
+          </Typography>
+          <Stack
+            direction="row"
+            spacing={1}
+            flexWrap="wrap"
+            style={{maxHeight: 400, overflowX: 'auto'}}
           >
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <TagIcon />
-                </ListItemIcon>
-                {isLoading ? (
-                  <Skeleton width={100} />
-                ) : (
-                  <ListItemText primary={name} />
-                )}
-              </ListItemButton>
-            </ListItem>
-          </Link>
-        ))}
-      </List>
-    </SideBlock>
+            {(items).map((name, index) => (
+              <Link key={index} to={`/tags/${name}`} style={{marginBottom: '15px'}}>
+                <Chip label={name} clickable onClick={handleClose} />
+              </Link>
+            ))}
+          </Stack>
+        </Box>
+      </Modal>
+    </>
   );
 };
